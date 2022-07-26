6479bf8669feacfe70e6157dd1867aac17302b57
vedjmehta <86235204+vedjmehta@users.noreply.github.com>
2022-07-26 19:27:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas();
+  canvas.position();
+  video = createCapture(VIDEO);
+  video.hide()
+}
+
+function draw() {
+  image()
+}
