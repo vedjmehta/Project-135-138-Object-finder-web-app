465bfc29c63ffdfe43d889d62e03ad19bde243ef
vedjmehta <86235204+vedjmehta@users.noreply.github.com>
2022-08-30 11:03:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,28 @@ function preload() {
 }
 
 function setup() {
-  canvas = createCanvas();
-  canvas.position();
+  canvas = createCanvas(480,380);
+  canvas.center();
   video = createCapture(VIDEO);
   video.hide()
 }
 
 function draw() {
-  image()
+  image( video, 0, 0, 480, 380);
+}
+
+
+video = "";
+
+
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd, modelLoaded');
+    document.getElementById("status").innerHTML = "status : Object Detecting";
+}
+
+function modelLoaded() {
+    console.log("model Loaded!");
+    status = true;
+   
 }
